58ebbf2b0d0ecbdb46b4bc9fe5bef377e43673b1
Tuija Sonkkila <tts@users.noreply.github.com>
2020-04-24 12:53:10 +0300

    Update parse.R

--- parse.R
+++ parse.R
@@ -9,10 +9,58 @@ source("calendarHeat.R")
 
 # From my requested Instagram data as of 2020-04-17
 likes <- fromJSON("likes.json")
+pics1 <- fromJSON("media.json")
+pics2 <- fromJSON("media2.json")
 
-# Comment likes are included too
-media_likes <- data.frame(likes$media_likes, stringsAsFactors = FALSE)
-names(media_likes) <- c("timestamp","account")
+# Images ---------------------------
+
+pics1_df <- data.frame(pics1$photos, stringsAsFactors = FALSE)
+pics2_df <- data.frame(pics2$photos, stringsAsFactors = FALSE)
+pics <- rbind(pics1_df, pics2_df)
+
+pics <- pics %>% 
+  mutate(timestamp_tidy = str_replace(taken_at, "\\+00\\:00", ""),
+         time = format(as.POSIXct(strptime(timestamp_tidy, format="%Y-%m-%dT%H:%M:%S")), format = "%H:%M"),
+         date = as.Date(taken_at)) %>% 
+  group_by(date) %>% 
+  tally() %>% 
+  ungroup()
+
+# Making two sets to get only one plot at a time
+older <- pics %>% 
+  filter(date <= "2015-12-31") %>% 
+  mutate(date = as.character(date))
+
+newer <- pics %>% 
+  filter(date > "2015-12-31") %>% 
+  mutate(date = as.character(date))
+
+# Exporting as PNG 800x700 from the GUI
+calendarHeat(dates = older$date, 
+             values = older$n, 
+             color="w2b", 
+             varname="My Instagram images")
+
+calendarHeat(dates = newer$date, 
+             values = newer$n, 
+             color="w2b", 
+             varname="My Instagram images")
+
+# Likes ---------------------------
+
+accounts <- fromJSON("connections.json")
+accounts_df <- data.frame(accounts$following, stringsAsFactors = FALSE)
+
+# Transpose
+following <- data.frame(name = names(accounts_df), 
+                        date = as.character(accounts_df[1, ]), 
+                        stringsAsFactors = FALSE)
+
+following <- following %>% 
+  mutate(date_tidy = str_replace(date, "\\+00\\:00", ""),
+         date = as.Date(date_tidy),
+         daysfollowed = as.integer(Sys.Date() - date)) %>% 
+  select(-date_tidy)
 
 # Whose photos have I liked the most, in total
 media_stat <- media_likes %>% 
@@ -38,7 +86,6 @@ media <- media_likes %>%
   tally() %>% 
   ungroup()
 
-# Making two sets to get only one plot at a time
 older <- media %>% 
   filter(date <= "2015-12-31") %>% 
   mutate(date = as.character(date))
@@ -47,7 +94,6 @@ newer <- media %>%
   filter(date > "2015-12-31") %>% 
   mutate(date = as.character(date))
 
-# Exporting both as PNG 800x700 from the GUI
 calendarHeat(dates = older$date, 
              values = older$n, 
              color="w2b", 
@@ -57,5 +103,3 @@ calendarHeat(dates = newer$date,
              values = newer$n, 
              color="w2b", 
              varname="My Instagram likes")
-
-
